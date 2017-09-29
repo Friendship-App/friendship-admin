fdf2a9f4f6c0af1e95c589e54268fd062ec61846
Eric <eric.mclaren2@gmail.com>
2017-09-29 15:58:10 +0300

    user page and started tags page

--- src/modules/Tags.jsx
+++ src/modules/Tags.jsx
@@ -1,103 +1,144 @@
 import React from 'react';
-
-import Card, { CardContent, CardActions, CardMedia } from 'material-ui/Card';
+import { connect } from 'react-redux';
+import { injectIntl } from 'react-intl';
 
 import Button from 'material-ui/Button';
-import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField'
+import Table, {
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from 'material-ui/Table';
 
-import CardGridWrapper from '../components/CardGridWrapper';
+import { LinearProgress } from 'material-ui/Progress';
+import ListIcon from 'material-ui-icons/List';
+import DeleteIcon from 'material-ui-icons/Delete';
+import WarningIcon from 'material-ui-icons/Warning';
 
-import theme from '../utils/theme';
+import { DialogContentText } from 'material-ui/Dialog';
+import DialogWithButtons from '../components/DialogWithButtons';
 
-import chilicorn from '../assets/chilicorn/chilicorn_no_text-256.png';
-import placeholder from '../assets/placeholder.png';
+import rest from '../utils/rest';
 
-const styles = {
-  chilicornHeader: {
-    height: 240,
-    background: `url(${chilicorn})`,
-    backgroundColor: theme.palette.primary[100],
-    backgroundSize: 'contain',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
+const mapStateToProps = state => ({
+  tags: state.tags,
+  tagsLoading: state.tags.loading,
+  tagDetails: state.tagDetails,
+});
+
+const mapDispatchToProps = dispatch => ({
+
+  /**
+   * Refresh the tags list
+   *
+   * @return {void}
+   */
+  refresh: () => {
+    dispatch(rest.actions.tags());
   },
-  loremHeader: {
-    height: 240,
-    background: `url(${placeholder})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    objectFit: 'cover',
-    width: '100%',
-  },
-};
 
-export default class Home extends React.Component {
-  renderChilicornCard = () =>
-    <Card>
-      <CardMedia>
-        <div style={styles.chilicornHeader} />
-      </CardMedia>
-      <CardContent>
-        <Typography type="headline" component="h2">
-          Title 1
-        </Typography>
+  /**
+   * Refresh a spcific user
+   *
+   * @param  {Object} tag The tag to be refreshed
+   * @return {void}
+   */
+   refreshTag: tag => {
+     dispatch(rest.actions.tagDetails({tagId: tag.id}))
+   }
+});
 
-        <Typography component="p">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          lobortis, diam id dapibus auctor, augue urna bibendum ligula, id
-          finibus est tortor vel dolor. Phasellus a nulla tellus. Phasellus
-          augue ante, consequat vel condimentum eu, vulputate vitae nulla. Morbi
-          ut finibus risus. Etiam gravida felis lectus, eu sagittis dolor auctor
-          et. Vivamus nec leo non ligula tincidunt vulputate quis efficitur mi.
-          In est eros, dignissim ut aliquet ut, ultrices eget nisi.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button compact color="primary">
-          Share
-        </Button>
-        <Button compact color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>;
+export class Tags extends React.Component {
+  // Component initial state.
+  // Here we keep track of whether the user details dialog is open.
+  state = {
+    dialogOpen: false,
+  };
 
-  renderPlaceholderCard = () =>
-    <Card>
-      <CardMedia>
-        <div style={styles.loremHeader} />
-      </CardMedia>
-      <CardContent>
-        <Typography type="headline" component="h2">
-          Title 2
-        </Typography>
+// Refresh user list when component is first mounted
+componentDidMount() {
+  const { refresh } = this.props;
+  refresh();
+}
 
-        <Typography component="p">
-          Proin odio dolor, aliquet ac tellus sit amet, blandit venenatis massa.
-          Phasellus id aliquet dui, eu rutrum lectus. Suspendisse hendrerit
-          sollicitudin mauris, sed venenatis augue tristique et. Proin sed
-          tortor lacinia, finibus diam eget, vulputate elit. Sed venenatis nunc
-          nec urna molestie aliquet a at tortor. Proin dignissim diam ac turpis
-          viverra auctor. Sed ac faucibus mauris, at consequat ipsum. Nunc
-          cursus nunc id augue aliquet, sed vulputate nisl commodo.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button compact color="primary">
-          Share
-        </Button>
-        <Button compact color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>;
+renderTagDetailsDesc = () =>
+  <div>
+    <DialogContentText>
+      <b>
+        {this.props.intl.formatMessage({ id: 'tagId'})}
+      </b>
+      {`: ${this.props.tagDetails.data.id}`}
+    </DialogContentText>
+  </div>;
 
+  /**
+   * Render the user row in the user list
+   *
+   * @param  {object} user The user that has to be rendered
+   * @return {TableRow} The tablerow associated with the user
+   */
+   renderTagRow = (tag) =>
+    <TableRow key={tag.id}>
+      <TableCell>
+        {tag.id}
+      </TableCell>
+      <TableCell>
+        {tag.name}
+      </TableCell>
+      <TableCell>
+        {tag.loves}
+      </TableCell>
+      <TableCell>
+        {tag.hates}
+      </TableCell>
+      <TableCell>
+        {tag.creator}
+      </TableCell>
+      <TableCell>
+        {tag.relatedEvents}
+      </TableCell>
+    </TableRow>;
+
+    /**
+     * Render the tag list
+     *
+     * @return {Node}
+     */
   render() {
-    return (
-      <CardGridWrapper>
-        {this.renderChilicornCard()}
-        {this.renderPlaceholderCard()}
-      </CardGridWrapper>
+    return(
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              {this.props.intl.formatMessage({ id: 'tagId' })}
+            </TableCell>
+            <TableCell>
+              {this.props.intl.formatMessage({ id: 'name' })}
+            </TableCell>
+            <TableCell>
+              {this.props.intl.formatMessage({ id: 'Loves' })}
+            </TableCell>
+            <TableCell>
+              {this.props.intl.formatMessage({ id: 'Hates' })}
+            </TableCell>
+            <TableCell>
+              {this.props.intl.formatMessage({ id: 'creator' })}
+            </TableCell>
+            <TableCell>
+              {this.props.intl.formatMessage({ id: 'related events' })}
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {// Loop over each user and render a <TableRow>
+          this.props.tags.data.map(tag =>
+            this.renderTagRow(tag)
+          )}
+        </TableBody>
+      </Table>
     );
   }
 }
+
+export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(Tags));
--- src/utils/rest.js
+++ src/utils/rest.js
@@ -35,6 +35,11 @@ const rest = reduxApi({
     transformer: transformers.array,
     crud: true,
   },
+  tags: {
+    url: `${apiRoot}/tags`,
+    transformer: transformers.array,
+    crud: true,
+  },
   userDetails: {
     url: `${apiRoot}/users/:userId`,
     crud: true,
