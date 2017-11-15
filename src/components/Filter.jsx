4dc37b07beff6ce92fbb7cbfaf0389c82e4e413c
Eran <e.machiels@hotmail.com>
2017-11-15 14:16:30 +0200

    Removed filter dialog

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,8 +11,6 @@ import IconButton from 'material-ui/IconButton';
 import { FormGroup, FormControlLabel } from 'material-ui/Form';
 import Checkbox from 'material-ui/Checkbox';
 
-import DialogWithButtons from './DialogWithButtons';
-
 import FilterListIcon from 'material-ui-icons/FilterList';
 
 import theme from '../utils/theme';
@@ -27,9 +25,7 @@ class Filter extends React.Component {
   }
 
   state = {
-    dialogOpen: false,
     value: '',
-    fields: []
   }
 
   handleChange = (event) => {
@@ -40,31 +36,6 @@ class Filter extends React.Component {
       value: event.target.value,
     });
   };
-
-  renderDialog = () =>
-    <div>
-      <DialogWithButtons
-        isOpen={this.state.dialogOpen}
-        submitAction="Submit"
-        description={this.renderDialogDesc()}
-        close={() => this.setState({dialogOpen: false})}
-        submit={() => this.setState({dialogOpen: false})}
-        />
-    </div>;
-
-    renderDialogDesc = () =>
-      this.state.fields.map((field) => {
-        return <FormControlLabel
-          control={
-            <Checkbox
-              checked={this.state.fields[field]}
-              onChange={this.handleChange('checkedA')}
-              value="checkedA"
-            />
-          }
-          label="Option A"
-        />
-      });
 
   render() {
     const {
@@ -77,7 +48,6 @@ class Filter extends React.Component {
     return (
       <div style={{display: 'flex'}}>
         <div style={theme.root}>
-          {this.renderDialog()}
           <Grid container>
             <Grid item xs={12}>
               <Paper style={theme.paper}>
@@ -87,11 +57,6 @@ class Filter extends React.Component {
                   margin="normal"
                   onChange={(event) => this.handleChange(event)}
                  />
-                 <IconButton
-                   style={{verticalAlign: 'middle'}}
-                   color="primary"
-                   onClick={() => this.setState({dialogOpen: true})}
-                   ><FilterListIcon /></IconButton>
                  <Button
                    color="primary"
                    key="submit"
